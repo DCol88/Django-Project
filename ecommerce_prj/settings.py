7a853ebea27a6ee728030caeb4f7a2da85c89809
Dave Colgan <dcolgan88@gmail.com>
2017-07-19 14:43:09 +0100

    Deploy code

--- ecommerce_prj/settings.py
+++ ecommerce_prj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,7 +46,10 @@ INSTALLED_APPS = [
     'payments',
     'cart',
     'categories',
-    'magazine'
+    'magazine',
+    'storages'
+    
+
 ]
 
 MIDDLEWARE = [
@@ -82,12 +87,16 @@ WSGI_APPLICATION = 'ecommerce_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+
 
 
 # Password validation
@@ -122,21 +131,41 @@ USE_L10N = True
 
 USE_TZ = True
 
+DISQUS_WEBSITE_SHORTNAME =  os.environ.get("DISQUS_SHORTNAME")
+SITE_ID = 1
 
-STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE_KEY')
-STRIPE_SECRET = os.getenv('STRIPE_SECRET_KEY')
+# AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+#    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#    'Cache-Control': 'max-age=94608000',
+# }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+# AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+# MEDIAFILES_LOCATION = 'media'
+# MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, "static"),
 )
+STATIC_URL = '/static/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.environ.get('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET = os.environ.get('STRIPE_SECRET_KEY')
 
 DEFAULT_FROM_EMAIL = 'davecommerce@example.com'
 
