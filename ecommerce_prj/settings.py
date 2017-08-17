0beaf8906e9cc82abebeeddfba9e515a5e0adecc
Dave Colgan <dcolgan88@gmail.com>
2017-08-17 13:08:14 +0100

    Configure for Postgres DB on Heroku

--- ecommerce_prj/settings.py
+++ ecommerce_prj/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'ecommerce_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 
-DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-      }
- }
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.sqlite3',
+#           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#       }
+#  }
 
 
 
